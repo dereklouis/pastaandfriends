45653f611c4f24c30f71da47439a91cf1779f2b2
Derek Louis <thedereklouis@gmail.com>
2021-05-21 13:59:22 -0400

    added hockey stick burger

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,6 +2,17 @@ import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const toggleBurger = () => {
+    const stick0 = document.getElementById('stick0');
+    const stick1 = document.getElementById('stick1');
+    if (stick0.className === 'stick0Close') {
+      stick0.className = 'stick0Open';
+      stick1.className = 'stick1Open';
+    } else {
+      stick0.className = 'stick0Close';
+      stick1.className = 'stick1Close';
+    }
+  };
   return (
     <div id="navBarWrapper">
       <Link className="navLink" to="/teamstats">
@@ -23,6 +34,20 @@ const NavBar = () => {
       <Link className="navLink" to="/">
         Four
       </Link>
+      <div id="stickContainer" onClick={toggleBurger}>
+        <img
+          src="stick.png"
+          className="stick0Close"
+          id="stick0"
+          alt="hockey stick"
+        />
+        <img
+          src="stick.png"
+          className="stick1Close"
+          id="stick1"
+          alt="hockey stick"
+        />
+      </div>
     </div>
   );
 };
